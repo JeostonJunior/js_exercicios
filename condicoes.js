c2ee9cc158ee28473a176a025e44170a0728b234
Jeoston Araujo <jeostonjunior@gmail.com>
2022-01-06 19:56:23 -0300

    completos

---
+++ condicoes.js
@@ -0,0 +1,10 @@
+var velocidade = 120
+
+console.log(`\nA velocidade do seu carro é ${velocidade}`)
+if (velocidade > 80) {
+    console.log(`\nA velocidade do seu carro está a acima da velocidade permitida\n`)
+    console.log('Você foi Multado!')
+}else {
+    console.log('Tenha uma boa viagem, utilize cinto de segurança\n')
+}
+
