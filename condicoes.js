9baeb60603023cb44f7f6a86459f2a378fd207df
Jeoston Araujo <jeostonjunior@gmail.com>
2022-01-09 04:02:29 -0300

    melhorias e condição parte 2

--- condicoes.js
+++ condicoes.js
@@ -1,10 +1,11 @@
-var velocidade = 120
+var velocidade = 120;
 
-console.log(`\nA velocidade do seu carro é ${velocidade}`)
+console.log(`\nA velocidade do seu carro é ${velocidade}`);
 if (velocidade > 80) {
-    console.log(`\nA velocidade do seu carro está a acima da velocidade permitida\n`)
-    console.log('Você foi Multado!')
-}else {
-    console.log('Tenha uma boa viagem, utilize cinto de segurança\n')
+  console.log(
+    `\nA velocidade do seu carro está a acima da velocidade permitida\n`
+  );
+  console.log("Você foi Multado!");
+} else {
+  console.log("Tenha uma boa viagem, utilize cinto de segurança\n");
 }
-
---
+++ condicoes_2.js
@@ -0,0 +1,10 @@
+var hora = 19;
+
+if(hora <= 12){
+    console.log('Boa Dia');
+}else if (hora > 12 && hora < 18){
+    console.log('Boa Tarde');
+}else{
+    console.log('Boa Noite');
+}
+
