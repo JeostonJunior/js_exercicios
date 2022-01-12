8b4f6d6ba2b3ed261c0f63676104d79a80d8b44b
Jeoston Araujo <jeostonjunior@gmail.com>
2022-01-12 15:26:45 -0300

    repetições

---
+++ repeticoes.js
@@ -0,0 +1,22 @@
+/*
+var c = 1;
+*/
+/*
+while (c <= 10) {
+  console.log(`Passo ${c}`);
+  c++;
+}
+*/
+/*
+do {
+  console.log(`Passo ${c}`);
+  c++;
+} while (c <= 10);
+*/
+
+/*
+for (var c = 1; c <= 10; c++) {
+  console.log(`Passo ${c}`);
+}
+
+*/
