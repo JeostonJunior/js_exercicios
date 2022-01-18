ebbb493a679ed48e18fd54c5378ef5431303fc70
Jeoston Araujo <jeostonjunior@gmail.com>
2022-01-18 19:00:48 -0300

    Corrigindo Erros

--- Analisador de Números/analisador.js
+++ Analisador de Números/analisador.js
@@ -1,10 +1,13 @@
 const lista = [];
 const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+var num = document.getElementById("txtnum");
+var analisar = document.getElementById("analisar");
+var final = document.getElementById("finalizar");
+
 function adicionar() {
-  var num = document.getElementById("txtnum");
-  let analisar = document.getElementById("analisar");
-  let numero = Number(num.value);
-  let listagem = "";
+  var numero = Number(num.value);
+  var listagem = "";
 
   if (num.value.length == 0 || numero <= 0 || numero > 100) {
     window.alert("Por favor, digite um número válido!");
@@ -20,20 +23,25 @@ function adicionar() {
     }
   }
   analisar.innerHTML += listagem;
+  num.value = "";
+  num.focus();
 }
 
 function finalizar() {
-  let finalizar = document.getElementById("finalizar");
-  let tamanho = lista.length;
   lista.sort();
+  let tamanho = lista.length;
   let min = Math.min(...lista);
   let max = Math.max(...lista);
   let total = lista.reduce(reducer);
   let media = total / tamanho;
-
-  finalizar.innerHTML = `Ao todo, temos ${tamanho} numeros cadastrados <br>`;
-  finalizar.innerHTML += `O maior valor informado foi ${max} <br>`;
-  finalizar.innerHTML += `O menor valor informado foi ${min}<br>`;
-  finalizar.innerHTML += `Somando todos os valores, temos ${total}<br>`;
-  finalizar.innerHTML += `A media dos valores digitados é ${media}<br>`;
+  if (lista.length == 0) {
+    window.alert("Adicione Valores Antes de Finalizar");
+  } else {
+    final.innerHTML = " ";
+    final.innerHTML += `Ao todo, temos ${tamanho} números cadastrados <br><br>`;
+    final.innerHTML += `O maior valor informado foi ${max} <br><br>`;
+    final.innerHTML += `O menor valor informado foi ${min}<br><br>`;
+    final.innerHTML += `Somando todos os valores, temos ${total}<br><br>`;
+    final.innerHTML += `A media dos valores digitados é ${media}<br>`;
+  }
 }
